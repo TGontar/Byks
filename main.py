18ee9e387467dbb4681b41cb025dff3762963ce5
TGontar <gontar.ts@mail.ru>
2021-12-18 01:53:34 +0300

    added players

--- main.py
+++ main.py
@@ -5,26 +5,9 @@ from _thread import *
 import threading
 
 
-# print_lock = threading.Lock()
-
-
-# sock = socket.socket()
-# sock.bind(('', 9090))
-# a = 10
-# sock.listen(a)
-# conn, addr = sock.accept()
-# print('connected:', addr)
-# lis = random.sample(range(1, 10), 4)
-# number = ''.join(map(str, lis))
-# bnumber = bytes(str(number), encoding="UTF-8")
-# print(number)
-
-def threaded(conn):
-    # print('Thread: ' + str(conn))
-    lis = random.sample(range(1, 10), 4)
-    number = ''.join(map(str, lis))
-    bnumber = bytes(str(number), encoding="UTF-8")
-    print('Answer ' + number)
+def threaded(conn, number):
+    name = "PLAYER #" + str(random.randint(1, 1000))
+    print(name + " joined the game")
     while True:
         message = ''
         data = conn.recv(1024)
@@ -32,27 +15,30 @@ def threaded(conn):
             break
         else:
             data = data.decode(encoding='UTF-8')
-            print(data)
+            print(name + ": " + data)
             message = byki.byks(data, number)
             conn.send(bytes(message, encoding="UTF-8"))
-        print(message)
+        print(name + ": " + message)
         if message == 'HAROSH TI POBEDIL':
             # print_lock.release()
             break
+
+
 def Main():
+    lis = random.sample(range(1, 10), 4)
+    number = ''.join(map(str, lis))
+    bnumber = bytes(str(number), encoding="UTF-8")
+    print('Answer ' + number)
     sock = socket.socket()
     sock.bind(('', 9090))
     a = 10
     sock.listen(a)
+    print('IGRA V KALMARA NACHALAS`')
     while True:
         conn, addr = sock.accept()
-        print('connected:', addr)
-        # print_lock.acquire()
-        # print('Connected to :', addr[0], ':', addr[1])
-        start_new_thread(threaded, (conn,))
-        # t1 = threading.Thread(target=threaded(conn))
-        # t1.start()
-    # conn.close()
-# conn.close()
+        # print('connected:', addr)
+        start_new_thread(threaded, (conn, number))
+
+
 if __name__ == '__main__':
     Main()
