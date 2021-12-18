94f44976c456cab3796f4eb4f5cc3d8c63b7258d
TGontar <gontar.ts@mail.ru>
2021-12-18 12:58:17 +0300

    privetstvie added

--- client.py
+++ client.py
@@ -2,6 +2,10 @@ import socket
 sock = socket.socket()
 sock.connect((str(input()), 9090))
 message = ''
+print("""DOBRO POZHALOVAT V IGRY BYKI I KOROVI. 
+TSEL PROSTA: OTGADAT` 4 ZNACHNOE CHISLO, ZAGADANNOY KOMPOM, 
+ESLI VI UGADALI TSIFRY I EE POZITSIY, TO ETO BYK. ESLI UGADALI TSIFRY, 
+NO POZITSIA DRYGAYA, TO ETO KOROVA""")
 while message != 'HAROSH TI POBEDIL':
     sock.send(bytes(str(input()), encoding='UTF-8'))
     data = sock.recv(1024)
